fd6b8adbcf428e3de8a0dc05ea05831acad6d267
root <root@ip-172-31-53-65.ec2.internal>
2021-02-11 08:50:31 +0000

    this is sample project

---
+++ myapp/maventestapp/src/main/java/org/bharath/com/App.java
@@ -0,0 +1,16 @@
+package org.bharath.com;
+
+import org.slf4j.*;
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+     
+        Logger logger = LoggerFactory.getLogger(App.class);
+        logger.info("hello world");
+    }
+}
